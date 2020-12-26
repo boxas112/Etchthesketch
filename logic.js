b02abcb256f0a0fca84b58760509b624c7138905
boxas112 <ernis144@gmail.com>
2020-12-26 14:20:14 +0200

    Added logic

--- logic.js
+++ logic.js
@@ -5,9 +5,8 @@ function makeRows(rows, cols) {
   container.style.setProperty('--grid-cols', cols);
   for (c = 0; c < (rows * cols); c++) {
     let cell = document.createElement("div");
-    cell.innerText = (c + 1);
     container.appendChild(cell).className = "grid-item";
   };
 };
 
-makeRows(5, 5);
+makeRows(16, 16);
