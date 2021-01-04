2ef07e656953dfe672a2d93eb9f0949deadf7ef5
boxas112 <ernis144@gmail.com>
2021-01-04 23:39:08 +0200

    Trying to make a clear grid button/not finished yet

--- logic.js
+++ logic.js
@@ -1,4 +1,5 @@
 const container = document.getElementById("container");
+const resetbutton = document.getElementById("resetbtn");
 
 function makeRows(rows, cols) {
   container.style.setProperty('--grid-rows', rows);
@@ -9,12 +10,30 @@ function makeRows(rows, cols) {
     container.addEventListener("mouseover", changeColor);
   };
 };
+
 function changeColor(e) {
   const randomR = Math.floor(Math.random() * 256);
   const randomG = Math.floor(Math.random() * 256);
   const randomB = Math.floor(Math.random() * 256);
-  e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
+  e.target.style.backgroundColor = `rgb(0,0,0)`;
+};
+
+function clearGrid() {
+  const gridArray = Array.from(container.childNodes)
+    gridArray.forEach((element) => {
+      container.removeChild(element);
+  let gridRows = prompt("Enter how much rows you want in your grid :");
+  let gridCols = prompt("Enter how much columns you want in your grid :");
+  makeRows(gridRows,gridCols);
+  
+    });
 }
+
+resetbutton.addEventListener("click", clearGrid);
+
+
+
+
 
 
 makeRows(16, 16);
