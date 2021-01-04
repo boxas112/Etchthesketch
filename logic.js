58c3f1017282f09dd8b2d5dda5fbafda262d9e68
boxas112 <ernis144@gmail.com>
2021-01-04 21:57:59 +0200

    Added coloring logic

--- logic.js
+++ logic.js
@@ -5,8 +5,19 @@ function makeRows(rows, cols) {
   container.style.setProperty('--grid-cols', cols);
   for (c = 0; c < (rows * cols); c++) {
     let cell = document.createElement("div");
-    container.appendChild(cell).className = "grid-item";
+    container.appendChild(cell).className = "grid-item grid-item-hover";
+    container.addEventListener("mouseover", changeColor);
   };
 };
+function changeColor(e) {
+  const randomR = Math.floor(Math.random() * 256);
+  const randomG = Math.floor(Math.random() * 256);
+  const randomB = Math.floor(Math.random() * 256);
+  e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
+}
+
 
 makeRows(16, 16);
+
+
+
